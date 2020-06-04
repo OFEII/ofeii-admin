fee31ea450ace14343a11ad6472a3df55675ab8e
OFEII <2282252066@qq.com>
2020-06-04 16:11:03 +0800

    +new: home主页的基本布局layout实现

--- ofeii-admin/src/components/Home.vue
+++ ofeii-admin/src/components/Home.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+
+  <!-- <el-container>
+    <div>
+  <el-button type="info" @click="logout">退出</el-button>
+</div>
+  </el-container> -->
+  <el-container class="home-container">
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -21,6 +34,15 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.home-container{
+  height: 100%;
+}
+.el-header{
+  background-color: rgb(98, 96, 96);
+}
+.el-aside{
+  background-color: rgb(122, 118, 118);
+}
 
 </style>
