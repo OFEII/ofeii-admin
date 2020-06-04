a9d6318a1ad5867ae20fae7572e57df282d43b78
OFEII <2282252066@qq.com>
2020-06-04 17:04:19 +0800

    +new: axios请求拦截器添加token 保证拥有获取数据的权限

--- ofeii-admin/src/main.js
+++ ofeii-admin/src/main.js
@@ -9,7 +9,15 @@ import './assets/fonts/iconfont.css'
 
 import axios from 'axios'
 axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
+// axios请求拦截
+axios.interceptors.request.use(config=>{
+  console.log(config)
+  // 为请求头对象添加token验证的authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
+
 
 Vue.use(axios);
 Vue.use(ElementUI);
