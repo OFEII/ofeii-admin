7d933c73e63c62fcef7d572f4394e06fd6a00401
OFEII <2282252066@qq.com>
2020-06-04 08:25:53 +0800

    +new: 登录组件的布局及图标引入

---
+++ ofeii-admin/src/components/Login.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="login-container">
+    <div class="login-box">
+      <div class="avatar-box">
+        <img src="../assets/logo.png" alt="logo-login" />
+      </div>
+      <!-- 登录表单form -->
+      <el-form class="login-form">
+        <!-- 用户名 -->
+        <el-form-item>
+          <el-input prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prefix-icon="iconfont icon-3702mima">
+          <el-input></el-input>
+        </el-form-item>
+        <!-- 登录按钮 -->
+        <el-form-item class="login-btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="primary">重置</el-button>  
+        </el-form-item>
+      </el-form>
+
+    </div>登录组件
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.login-container {
+  height: 100vh;
+  background-color: #2b4b6b;
+}
+
+.login-box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 8px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar-box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 1px 1px 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+}
+.login-form{
+  width: 80%;
+  position: absolute;
+  bottom: 0;
+  padding: 0 40px;
+}
+.login-btns{
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
