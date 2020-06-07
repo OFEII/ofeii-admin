1706667bc452bd287f3412c5b5a9ad1d95d42e25
OFEII <2282252066@qq.com>
2020-06-07 10:30:15 +0800

    +new: 权限列表-绘制面包屑和卡片视图

--- ofeii-admin/src/components/power/Rights.vue
+++ ofeii-admin/src/components/power/Rights.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-权限组件
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+      123
+    </el-card>
   </div>
 </template>
 
