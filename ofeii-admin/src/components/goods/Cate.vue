db4051a03cca68da86e2efdbf622798edbf533ad
OFEII <2282252066@qq.com>
2020-06-08 20:30:21 +0800

    +new: 商品分类-绘制商品分类的组件的基本页面布局

--- ofeii-admin/src/components/goods/Cate.vue
+++ ofeii-admin/src/components/goods/Cate.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-商品分类组件
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row>
+        <el-col :span="4">
+          <el-button type="primary">添加商品</el-button>
+        </el-col>
+      </el-row>
+      <!-- 表格 -->
+      <!-- 分页区域 -->
+    </el-card>
   </div>
 </template>
 
