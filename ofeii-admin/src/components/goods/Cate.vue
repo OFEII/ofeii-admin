ef8eeeced641f9954e713fd5dc6eea586c7b8d64
OFEII <2282252066@qq.com>
2020-06-08 22:47:00 +0800

    +new: 商品分类-实现分页功能

--- ofeii-admin/src/components/goods/Cate.vue
+++ ofeii-admin/src/components/goods/Cate.vue
@@ -9,12 +9,13 @@
     <!-- 卡片视图区域 -->
     <el-card>
       <el-row>
-        <el-col :span="4">
+        <el-col :span="2">
           <el-button type="primary">添加商品</el-button>
         </el-col>
       </el-row>
       <!-- 表格 -->
       <tree-table
+        class="treeTable"
         :data="catelist"
         :columns="columns"
         :selection-type="false"
@@ -22,25 +23,39 @@
         show-index
         index-text="#"
         border
-        :show-row-hover="false">
+        :show-row-hover="false"
+      >
         <!-- template 是否有效 -->
-        <template v-slot:isok="scope"> 
-          <i class="el-icon-success" v-if="scope.row.cat_deleted === false" style="color: lightgreen;"></i>
+        <template v-slot:isok="scope">
+          <i
+            class="el-icon-success"
+            v-if="scope.row.cat_deleted === false"
+            style="color: lightgreen;"
+          ></i>
           <i class="el-icon-error" v-else style="color: red;"></i>
         </template>
         <!-- template 是序 -->
-        <template v-slot:order="scope"> 
+        <template v-slot:order="scope">
           <el-tag size="mini" v-if="scope.row.cat_level === 0">一级</el-tag>
           <el-tag type="success" size="mini" v-else-if="scope.row.cat_level === 1">二级</el-tag>
           <el-tag type="warning" size="mini" v-else>三级</el-tag>
         </template>
         <!-- template 操作 -->
-        <template v-slot:option="scope"> 
+        <template v-slot:option="scope">
           <el-button type="primary" icon="el-icon-edit">编辑</el-button>
           <el-button type="danger" icon="el-icon-delete">删除</el-button>
         </template>
       </tree-table>
       <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="querInfo.pagenum"
+        :page-sizes="[3, 5, 10, 25]"
+        :page-size="querInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
     </el-card>
   </div>
 </template>
@@ -49,68 +64,80 @@
 export default {
   data() {
     return {
-      querInfo:{
-        type:3,
-        pagenum:1,
-        pagesize:5
+      querInfo: {
+        type: 3,
+        pagenum: 1,
+        pagesize: 5
       },
       // 商品分类的数据列表 默认为空
-      catelist:[],
+      catelist: [],
       // 总数据条数
-      total:0,
+      total: 0,
       // 为table指定列的定义
-      columns:[
+      columns: [
         {
-          label:'分类名称',
-          prop:'cat_name'
-        },{
-          label:'是否有效',
+          label: "分类名称",
+          prop: "cat_name"
+        },
+        {
+          label: "是否有效",
           // 表示将当前列定义为模版列
-          type:'template',
+          type: "template",
           // 表示当前这一列使用模版的名称
-          template:'isok'
-        },{
-          label:'排序',
+          template: "isok"
+        },
+        {
+          label: "排序",
           // 表示将当前列定义为模版列
-          type:'template',
+          type: "template",
           // 表示当前这一列使用模版的名称
-          template:'order'
-        },{
-          label:'操作',
+          template: "order"
+        },
+        {
+          label: "操作",
           // 表示将当前列定义为模版列
-          type:'template',
+          type: "template",
           // 表示当前这一列使用模版的名称
-          template:'option'
+          template: "option"
         }
-
       ]
-    }
+    };
   },
   created() {
-    this.getCateList()
+    this.getCateList();
   },
   methods: {
     // 获取商品分类数据
-    async getCateList(){
-      const {data:res} = await this.$http.get('categories',{
+    async getCateList() {
+      const { data: res } = await this.$http.get("categories", {
         params: this.querInfo
-      })
+      });
 
-      if(res.meta.status !== 200){
-        return this.$message.error('获取商品分类失败')
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取商品分类失败");
       }
-      console.log(res.data)
-      this.catelist = res.data.result
-      this.total = res.data.total
-      console.log(this.catelist)
-      console.log(this.total)
-
+      console.log(res.data);
+      this.catelist = res.data.result;
+      this.total = res.data.total;
+      console.log(this.catelist);
+      console.log(this.total);
+    },
+    // 监听pagesize的变化
+    handleSizeChange(newSize){
+      this.querInfo.pagesize = newSize
+      this.getCateList()
+    },
+    // 监听pagenums 变化
+    handleCurrentChange(newPage){
+      this.querInfo.pagenum = newPage
+      this.getCateList()
     }
-  },
-
-}
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
+.treeTable{
+  margin-top: 1rem;
+}
 </style>
