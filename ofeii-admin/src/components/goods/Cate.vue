6d2643196e0119d79f8f29385f10d60a27b794cc
OFEII <2282252066@qq.com>
2020-06-08 20:26:19 +0800

    +new: 商品分类-通过路由加载商品分类组件

---
+++ ofeii-admin/src/components/goods/Cate.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+商品分类组件
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  created() {
+    
+  },
+  methods: {
+    
+  },
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
