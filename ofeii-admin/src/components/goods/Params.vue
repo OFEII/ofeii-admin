1f870a2c5b4155f8756f97b388f4f5d242ba593d
OFEII <2282252066@qq.com>
2020-06-09 22:37:06 +0800

    +new: 分类参数-渲染分类参数页面的基本UI结构

--- ofeii-admin/src/components/goods/Params.vue
+++ ofeii-admin/src/components/goods/Params.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
-分类参数
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item></el-breadcrumb-item>
+      <el-breadcrumb-item>参数列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <!-- 警告区域 -->
+      <el-alert
+        title="注意：只允许为第三级分类设置相关参数"
+        type="warning"
+        :closeable="false"
+        show-icon>
+      </el-alert>
+      <!-- 选择商品分类区 -->
+      <el-row class="cat_opt" justify="start">
+        <el-col>
+          <span>选择商品分类：</span>
+          <!-- 选择商品级联的选择框 -->
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
@@ -22,5 +44,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.cat_pot{
+  margin: 1rem 0;
+}
 </style>
