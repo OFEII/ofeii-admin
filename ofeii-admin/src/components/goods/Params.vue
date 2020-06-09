c9db37e1cf53c72d8a18d957c0c1427e3e685da4
OFEII <2282252066@qq.com>
2020-06-09 22:22:22 +0800

    +new: 分类参数-通过路由加载分类参数组件页面

---
+++ ofeii-admin/src/components/goods/Params.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+分类参数
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  created() {
+    
+  },
+  methods: {
+    
+  },
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
