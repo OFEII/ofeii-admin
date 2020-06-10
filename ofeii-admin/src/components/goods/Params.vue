f59ff11b9bc22abbfa617079228a26cd9523713b
OFEII <2282252066@qq.com>
2020-06-10 09:54:56 +0800

    +new: 分类参数-渲染添加参数按钮并控制按钮的禁用状态

--- ofeii-admin/src/components/goods/Params.vue
+++ ofeii-admin/src/components/goods/Params.vue
@@ -32,8 +32,14 @@
 
       <!-- tab 页签区域 -->
       <el-tabs v-model="activeName" @tab-click="handleTabsClick">
-        <el-tab-pane label="动态参数" name="first">动态参数</el-tab-pane>
-        <el-tab-pane label="静态属性" name="second">静态属性</el-tab-pane>
+        <!-- 添加动态参数的面板 -->
+        <el-tab-pane label="动态参数" name="first">
+          <el-button type="primary" size="mini" :disabled="isBtnDisabled">添加参数</el-button>
+        </el-tab-pane>
+        <!-- 添加静态属性的面板 -->
+        <el-tab-pane label="静态属性" name="second">
+          <el-button type="primary" size="mini" :disabled="isBtnDisabled">添加属性</el-button>
+        </el-tab-pane>
       </el-tabs>
     </el-card>
   </div>
@@ -60,6 +66,11 @@ export default {
   },
   created() {
     this.getCateList()
+  },
+  computed: {
+    isBtnDisabled(){
+      return (this.selectedCateKeys.length !== 3)? true : false
+    }
   },
   methods: {
     async getCateList(){
