872a1ebffab4bba549d1503364bb7988799763d0
OFEII <2282252066@qq.com>
2020-06-11 09:06:55 +0800

    +new: 商品列表-自定义全局化的时间过滤器

--- ofeii-admin/src/components/goods/List.vue
+++ ofeii-admin/src/components/goods/List.vue
@@ -30,16 +30,17 @@
         <el-table-column label="商品名称" prop="goods_name"></el-table-column>
         <el-table-column label="商品价格(元)" prop="goods_price" width="140px"></el-table-column>
         <el-table-column label="商品重量(kg)" prop="goods_weight" width="140px"></el-table-column>
-        <el-table-column label="创建时间" prop="add_time" width="200px"></el-table-column>
+        <el-table-column label="创建时间" prop="add_time" width="200px">
+          <template v-slot="scope">
+            {{scope.row.add_time | dateFormat}}    
+          </template>
+        </el-table-column>
         <el-table-column label="操作" width="400px">
           <template v-slot="scope">
             <el-button type="primary" icon="el-icon-edit"></el-button>
             <el-button type="danger" icon="el-icon-delete"></el-button>
           </template>
         </el-table-column>
-
-
-
       </el-table>
     </el-card>
   </div>
