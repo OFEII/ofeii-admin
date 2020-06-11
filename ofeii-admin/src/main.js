54b7c62ac056a52abe7ec10031451b6632166f0f
OFEII <2282252066@qq.com>
2020-06-11 09:07:03 +0800

    +new: 商品列表-自定义全局化的时间过滤器

--- ofeii-admin/src/main.js
+++ ofeii-admin/src/main.js
@@ -21,6 +21,18 @@ axios.interceptors.request.use(config=>{
 })
 Vue.prototype.$http = axios
 
+// 时间全局过滤器
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth()+1+'').padStart(2, '0')
+  const d= (dt.getDate()+'').padStart(2, '0')
+  const hh = (dt.getHours()+'').padStart(2, '0')
+  const mm = (dt.getMinutes()+'').padStart(2, '0')
+  const ss = (dt.getSeconds()+'').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
 
 Vue.use(axios);
 Vue.use(ElementUI);
