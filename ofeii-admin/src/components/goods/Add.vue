09f0d02d820afeb9ef5bcf902b943af5204e101b
OFEII <2282252066@qq.com>
2020-06-13 10:30:46 +0800

    +new: 添加商品-渲染tab区域

--- ofeii-admin/src/components/goods/Add.vue
+++ ofeii-admin/src/components/goods/Add.vue
@@ -25,6 +25,15 @@
           <el-step title="商品内容"></el-step>
           <el-step title="完成"></el-step>
         </el-steps>
+
+        <!-- tab区域 -->
+        <el-tabs :tab-position="tabPosition" style="height: 200px;">
+          <el-tab-pane label="基本信息">基本信息</el-tab-pane>
+          <el-tab-pane label="商品参数">商品参数</el-tab-pane>
+          <el-tab-pane label="商品属性">商品属性</el-tab-pane>
+          <el-tab-pane label="商品图片">商品图片</el-tab-pane>
+          <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+        </el-tabs>
     </el-card>
 
   </div>
@@ -34,7 +43,8 @@
 export default {
   data() {
     return {
-      activeIndex: 0
+      activeIndex: 0,
+      tabPosition: 'left'
     }
   },
   computed: {
