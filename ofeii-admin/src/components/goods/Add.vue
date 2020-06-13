164797cd1df084d5e4ce8f9f67c0244ce1d4515f
OFEII <2282252066@qq.com>
2020-06-13 10:21:21 +0800

    +new: 添加商品-渲染添加商品页面的基本结构

--- ofeii-admin/src/components/goods/Add.vue
+++ ofeii-admin/src/components/goods/Add.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
-add goods page
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+        <!-- 警告提示区域 -->
+        <el-alert
+          title="添加商品信息"
+          type="info"
+          center
+          show-icon
+          :closable="false">
+        </el-alert>
+        <!-- 含状态步骤条 -->
+        <el-steps :space="200" :active="1" finish-status="success">
+          <el-step title="已完成"></el-step>
+          <el-step title="进行中"></el-step>
+          <el-step title="步骤 3"></el-step>
+        </el-steps>
+    </el-card>
+
   </div>
 </template>
 
@@ -22,5 +45,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.el-steps{
+  margin-top: 2rem;
+}
 
 </style>
