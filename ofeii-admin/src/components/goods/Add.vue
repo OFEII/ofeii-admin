8dcbfa50ca1ef46b610a46a598a086d8bf7c0c2f
OFEII <2282252066@qq.com>
2020-06-13 10:58:30 +0800

    +new: 添加商品-绘制步骤条1的基本信息

--- ofeii-admin/src/components/goods/Add.vue
+++ ofeii-admin/src/components/goods/Add.vue
@@ -25,15 +25,32 @@
           <el-step title="商品内容"></el-step>
           <el-step title="完成"></el-step>
         </el-steps>
-
+        <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="100px" label-position="top">
         <!-- tab区域 -->
-        <el-tabs v-model="activeIndex" :tab-position="tabPosition" style="height: 200px;">
-          <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
-          <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
-          <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
-          <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
-          <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
-        </el-tabs>
+          <el-tabs v-model="activeIndex" :tab-position="tabPosition">
+            <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
+              <el-form-item label="商品名称" prop="goods_name">
+                <el-input v-model="addForm.goods_name"></el-input>
+              </el-form-item>
+              <el-form-item label="商品价格(元)" prop="goods_price">
+                <el-input v-model="addForm.goods_price" type="number"></el-input>
+              </el-form-item>
+              <el-form-item label="商品重量(kg)" prop="goods_weight">
+                <el-input v-model="addForm.goods_weight" type="number"></el-input>
+              </el-form-item>
+              <el-form-item label="商品数量" prop="goods_number">
+                <el-input v-model="addForm.goods_number" type="number"></el-input>
+              </el-form-item>
+            <el-tab-pane label="商品参数" name="1">商品参数
+            </el-tab-pane>
+            <el-tab-pane label="商品属性" name="2">商品属性
+            </el-tab-pane>
+            <el-tab-pane label="商品图片" name="3">商品图片
+            </el-tab-pane>
+            <el-tab-pane label="商品内容" name="4">商品内容
+            </el-tab-pane>
+          </el-tabs>
+        </el-form>
     </el-card>
 
   </div>
@@ -44,7 +61,22 @@ export default {
   data() {
     return {
       activeIndex: 0,
-      tabPosition: 'left'
+      tabPosition: 'left',
+      addForm:{
+        goods_name:'',
+        goods_price: 0,
+        goods_weight: 0,
+        goods_number: 0,
+
+
+      },
+      addFormRules:{
+        goods_name:{required:true,message:'请输入商品名称',trigger:'blur'},
+        goods_price:{required:true,message:'请输入商品价格',trigger:'blur'},
+        goods_weight:{required:true,message:'请输入商品重量',trigger:'blur'},
+        goods_number:{required:true,message:'请输入商品数量',trigger:'blur'},
+
+      }
     }
   },
   computed: {
