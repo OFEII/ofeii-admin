7ecdc914b857b27ec63d0d641c7b50be0600dec0
OFEII <2282252066@qq.com>
2020-06-13 10:27:26 +0800

    +new: 添加商品-美化步骤条组件

--- ofeii-admin/src/components/goods/Add.vue
+++ ofeii-admin/src/components/goods/Add.vue
@@ -17,10 +17,13 @@
           :closable="false">
         </el-alert>
         <!-- 含状态步骤条 -->
-        <el-steps :space="200" :active="1" finish-status="success">
-          <el-step title="已完成"></el-step>
-          <el-step title="进行中"></el-step>
-          <el-step title="步骤 3"></el-step>
+        <el-steps :space="200" :active="activeIndex" finish-status="success" align-center >
+          <el-step title="基本信息"></el-step>
+          <el-step title="商品参数"></el-step>
+          <el-step title="商品属性"></el-step>
+          <el-step title="商品图片"></el-step>
+          <el-step title="商品内容"></el-step>
+          <el-step title="完成"></el-step>
         </el-steps>
     </el-card>
 
@@ -31,7 +34,7 @@
 export default {
   data() {
     return {
-      
+      activeIndex: 0
     }
   },
   computed: {
@@ -46,7 +49,7 @@ export default {
 
 <style lang="scss" scoped>
 .el-steps{
-  margin-top: 2rem;
+  margin: 2rem 0;
 }
 
 </style>
