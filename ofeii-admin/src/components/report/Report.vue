00c4d75445b185d139076c599d53ebae423c5a7d
OFEII <2282252066@qq.com>
2020-06-14 20:31:41 +0800

    +new: 数据报表-调用api接口完成数据报表的展示

--- ofeii-admin/src/components/report/Report.vue
+++ ofeii-admin/src/components/report/Report.vue
@@ -8,7 +8,7 @@
     </el-breadcrumb>
     <!-- 卡片视图区域 -->
     <el-card>
-      <div id="main" style="width: 600px;height:400px;"></div>
+      <div id="main" style="width: 800px;height:400px;"></div>
     </el-card>
   </div>
 </template>
@@ -19,6 +19,37 @@ import echarts from 'echarts'
 export default {
   data() {
     return {
+      // 需要合并的对象
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
       
     }
   },
@@ -26,33 +57,20 @@ export default {
     
   },
   // 此时页面的DOM元素已经被渲染完毕了
-  mounted() {
+  async mounted() {
     
     // 基于准备好的dom，初始化echarts实例
     var myChart = echarts.init(document.getElementById('main'));
+    const {data:res} = await this.$http.get('reports/type/1')
+    if(res.meta.status !== 200){
+      return this.$message.error('获取折线图数据失败！')
+    }
 
     // 指定图表的配置项和数据
-    var option = {
-        title: {
-            text: 'ECharts 入门示例'
-        },
-        tooltip: {},
-        legend: {
-            data:['销量']
-        },
-        xAxis: {
-            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-        },
-        yAxis: {},
-        series: [{
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-        }]
-    };
+    const result =  _.merge(res.data, this.options)
 
     // 使用刚指定的配置项和数据显示图表。
-    myChart.setOption(option);
+    myChart.setOption(result);
   },
   methods: {
     
