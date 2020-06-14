6fdf7a2d48f7fb6c93e12e112a127d61d567fd45
OFEII <2282252066@qq.com>
2020-06-14 20:32:35 +0800

    +new: 数据报表-完成了数据报表的功能

--- ofeii-admin/src/components/report/Report.vue
+++ ofeii-admin/src/components/report/Report.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-import echarts from 'echarts'
+import echarts from "echarts";
 
 export default {
   data() {
@@ -22,21 +22,21 @@ export default {
       // 需要合并的对象
       options: {
         title: {
-          text: '用户来源'
+          text: "用户来源"
         },
         tooltip: {
-          trigger: 'axis',
+          trigger: "axis",
           axisPointer: {
-            type: 'cross',
+            type: "cross",
             label: {
-              backgroundColor: '#E9EEF3'
+              backgroundColor: "#E9EEF3"
             }
           }
         },
         grid: {
-          left: '3%',
-          right: '4%',
-          bottom: '3%',
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
           containLabel: true
         },
         xAxis: [
@@ -46,39 +46,31 @@ export default {
         ],
         yAxis: [
           {
-            type: 'value'
+            type: "value"
           }
         ]
       }
-      
-    }
+    };
   },
-  created() {
-    
-  },
+  created() {},
   // 此时页面的DOM元素已经被渲染完毕了
   async mounted() {
-    
     // 基于准备好的dom，初始化echarts实例
-    var myChart = echarts.init(document.getElementById('main'));
-    const {data:res} = await this.$http.get('reports/type/1')
-    if(res.meta.status !== 200){
-      return this.$message.error('获取折线图数据失败！')
+    var myChart = echarts.init(document.getElementById("main"));
+    const { data: res } = await this.$http.get("reports/type/1");
+    if (res.meta.status !== 200) {
+      return this.$message.error("获取折线图数据失败！");
     }
 
     // 指定图表的配置项和数据
-    const result =  _.merge(res.data, this.options)
+    const result = _.merge(res.data, this.options);
 
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(result);
   },
-  methods: {
-    
-  },
-
-}
+  methods: {}
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
