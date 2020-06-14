083164bd231e10c8a17b0fdf3f9a42c3af987482
OFEII <2282252066@qq.com>
2020-06-14 20:23:07 +0800

    +new: 数据报表-安装echarts并渲染图表

--- ofeii-admin/src/components/report/Report.vue
+++ ofeii-admin/src/components/report/Report.vue
@@ -8,12 +8,14 @@
     </el-breadcrumb>
     <!-- 卡片视图区域 -->
     <el-card>
-      123
+      <div id="main" style="width: 600px;height:400px;"></div>
     </el-card>
   </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+
 export default {
   data() {
     return {
@@ -22,6 +24,35 @@ export default {
   },
   created() {
     
+  },
+  // 此时页面的DOM元素已经被渲染完毕了
+  mounted() {
+    
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'));
+
+    // 指定图表的配置项和数据
+    var option = {
+        title: {
+            text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+            data:['销量']
+        },
+        xAxis: {
+            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        },
+        yAxis: {},
+        series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
   },
   methods: {
     
